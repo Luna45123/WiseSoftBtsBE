24f58c9f4a24e7d67412ae7b122a03689cd30bd4
Sunny4826 <118628971+Luna45123@users.noreply.github.com>
2025-03-27 11:22:47 +0700

    change countStation

--- src/main/java/com/wisesoft/btsfare/service/CountStationService.java
+++ src/main/java/com/wisesoft/btsfare/service/CountStationService.java
@@ -11,34 +11,18 @@ public class CountStationService {
     
     public int countFareStations(List<Station> path) {
         int count = 0;
-        int extensionCount = 0;
+        boolean isOneExtensionCount = true;
         for (Station station : path) {
             if (station.isExtension()) {
-                if (extensionCount == 0) {
+                if (isOneExtensionCount) {
                     count++;
                 }
-                extensionCount++;
+                isOneExtensionCount = false;
             } else {
                 count++;
-                extensionCount = 0;
+                isOneExtensionCount = true;
             }
         }
-        // ลบ 1 เนื่องจากเริ่มต้นนับที่สถานีแรก (หรือปรับตาม business rule)
         return Math.max(0, count - 1);
     }
-    
-    // public int countFareStations(List<Station> path) {
-    //     int count = 0, extensionCount = 0;
-    //     for (Station station : path) {
-    //         if (station.isExtension()) {
-    //             if (extensionCount == 0) 
-    //             count++;
-    //             extensionCount++;
-    //         } else {
-    //             count++;
-    //             extensionCount = 0;
-    //         }
-    //     }
-    //     return Math.max(0, count - 1);
-    // }
 }
